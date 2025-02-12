15d41c907a105d8b052ace3b8b6e5064b3f7c4e6
flowcore-platform <gitops@flowcore.io>
2025-02-12 23:17:57 +0000

    fix: new version

--- test/tests/example.test.ts
+++ test/tests/example.test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "jsr:@std/assert/equals"
 import { describe, it } from "jsr:@std/testing/bdd"
 
 describe("Example", () => {
-  it("should be true", () => {
+  it("should be successful", () => {
     assertEquals(true, true)
   })
 })
