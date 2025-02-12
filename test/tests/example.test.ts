1611ae8e35a859c8aced6586c8addbec6d334f3c
flowcore-platform <gitops@flowcore.io>
2025-02-12 23:14:51 +0000

    fix: add example test file

---
+++ test/tests/example.test.ts
@@ -0,0 +1,8 @@
+import { assertEquals } from "jsr:@std/assert/equals"
+import { describe, it } from "jsr:@std/testing/bdd"
+
+describe("Example", () => {
+  it("should be true", () => {
+    assertEquals(true, true)
+  })
+})
