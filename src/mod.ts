6208e42bbf59e67d70235833a08d6e116b29dc60
flowcore-platform <gitops@flowcore.io>
2025-04-14 17:38:06 +0100

    feat(data-pump): :sparkles: export data source module

--- src/mod.ts
+++ src/mod.ts
@@ -2,5 +2,6 @@ export * from "./lib/data-pump.ts"
 export * from "./lib/data-pump-create.ts"
 
 export * from "./data-pump/data-pump.ts"
+export * from "./data-pump/data-source.ts"
 export * from "./data-pump/metrics.ts"
 export * from "./data-pump/types.ts"
